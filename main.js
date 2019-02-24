8f5cc4e2345ba55981b74e8238e7745a3c0c070d
Enrique Montemayor <eemontemayor@live.com>
2019-02-23 20:04:18 -0600

    working on appending

--- main.js
+++ main.js
@@ -6,26 +6,40 @@
 // sdfsdfdsf
 //  .submit(), preventDefault(), toggleClass(), and closest().
 
-function addItem(){
-   $('.js-shopping-list-form').submit(event =>{
-        event.preventDefault();
-        const userTextEl = $(event.currentTarget).find('#shopping-list-entry');
-        $(".shopping-list").append
 
-   }); 
-}
+
+// function returnAddItem(){
+//    $('.js-shopping-list-form').submit(event =>{
+//         event.preventDefault();
+//         const userTextEl = $(event.currentTarget).find('#shopping-list-entry');
+//         $("ul").append(
+//             '<li>
+//             <span class="shopping-item">+$(userTextEl)+</span>
+//             <div class="shopping-item-controls">
+//               <button class="shopping-item-toggle">
+//                 <span class="button-label">check</span>
+//               </button>
+//               <button class="shopping-item-delete">
+//                 <span class="button-label">delete</span>
+//               </button>
+//             </div>
+//           </li>'
+//         )
+
+//    }); 
+// }
 
 
 function checkItem () {
-    $("li").on('click', "button.shopping-item-toggle", function(event){
-       const item = $(event.target).closest('li'); // seriously ?! I would have that this toggles the other buttons as well so i tried everything else!
-        $(item).toggleClass('shopping-item__checked');
+    $("ul").on('click', "button.shopping-item-toggle", function(event){
+       const item = $(event.target).closest('li'); // why does  this not toggle the other classes in the li as well so i tried everything else!
+        $(item).toggleClass('shopping-item__checked'); // use second parameter for toggle class to check whether it has it or not
+        
     });
 }
 
 function removeItem(){
-    $('ul').on('click', 'button.shopping-item-delete', function(event){
-         
+    $('ul').on('click', 'button.shopping-item-delete', function(event){ 
         $(event.target).closest('li').remove();
     });
 }
